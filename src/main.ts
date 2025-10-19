30744b60bd332f477785fdf184818d4680507e20
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-10-19 17:25:51 -0300

    feat: enable uri versioning

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common'
+import { ValidationPipe, VersioningType } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
@@ -10,6 +10,7 @@ import type { Env } from './env'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.enableVersioning({ type: VersioningType.URI })
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
 
   const config = new DocumentBuilder()
