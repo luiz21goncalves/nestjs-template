7071ad506704f1848ad09e031ed4a2a787ce82d2
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-10-19 16:50:25 -0300

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { apiReference } from '@scalar/nestjs-api-reference'
 
 import { AppModule } from './app.module'
 import type { Env } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs template')
+    .setDescription('The NestJs template')
+    .setContact(
+      'Luiz Gonçalves',
+      'https://github.com/luiz21goncalves/nestjs-template',
+      'luizhbgoncalves@gmail.com'
+    )
+    .setVersion('0.0.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  app.use('/docs', apiReference({ content: document }))
 
   const configService = app.get<ConfigService<Env, true>>(ConfigService)
   const port = configService.get('PORT', { infer: true })
