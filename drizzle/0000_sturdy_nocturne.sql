df9a75a5dfca65deb454cbeb969127da4fbcdccf
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-11-20 19:22:22 -0300

    refactor: sort users table fields

--- drizzle/0000_sturdy_nocturne.sql
+++ drizzle/0000_sturdy_nocturne.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "users" (
-	"created_at" timestamp,
-	"email" text NOT NULL,
 	"id" text PRIMARY KEY NOT NULL,
+	"email" text NOT NULL,
+	"created_at" timestamp,
 	"updated_at" timestamp NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
